78573ae96db4c4fd762a6eed3358e6c69680547b
Shobhit Gupta <shobhit9618@gmail.com>
2020-10-08 18:18:59 +0530

    pip package 0.0.20

--- celeb_detector/create_model_utils.py
+++ celeb_detector/create_model_utils.py
@@ -14,10 +14,37 @@ import pickle
 import json
 from annoy import AnnoyIndex
 from tqdm import tqdm
+import json
 
 face_detector = MTCNN()
 encoder_model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
 # ann_index = AnnoyIndex(2048, 'angular')
+
+def load_indexes(ann_filepath=None, celeb_mapping_path=None):
+	home = expanduser("~")
+	if ann_filepath is None:
+		ann_filepath = os.path.join(home,'celeb_index_60.ann')
+		celeb_ann_id = '1-3Wb7fiINbrk9FSagTxjLdSjp7KzrMp7'
+		if not os.path.exists(ann_filepath):
+			download_file_from_google_drive(celeb_ann_id, ann_filepath)
+
+	if celeb_mapping_path is None:
+		celeb_mapping_path = os.path.join(home,'celeb_mapping.json')
+		celeb_mapping_file_id = '1wDaaSQ6NjxLkxpzYyTRknefizZUKnKDj'
+		if not os.path.exists(celeb_mapping_path):
+			download_file_from_google_drive(celeb_mapping_file_id, celeb_mapping_path)
+
+	ann_index = AnnoyIndex(2048, 'angular')
+	_ = ann_index.load(ann_filepath)
+
+	with open(celeb_mapping_path) as json_file:
+		celeb_mapping_temp = json.load(json_file)
+	celeb_mapping_dict = {}
+	for key, value_list in celeb_mapping_temp.items():
+		for each_id in value_list:
+			celeb_mapping_dict[each_id] = str(key)
+
+	return ann_index, celeb_mapping_dict
 
 def get_encoding(img_path):
 	img = pyplot.imread(img_path)
