0590959d8d375a4d61c73d573bd66a57464b07de
Shobhit Gupta <shobhit@accio.ai>
2021-11-09 20:45:36 +0530

    updates

--- hubconf.py
+++ hubconf.py
@@ -2,5 +2,5 @@ import torch
 
 from celeb_detector.celeb_recognition import celeb_recognition
 
-def find_celeb(image_path, url=False):
-	return celeb_recognition(image_path, url=False)
+def find_celeb(image_path):
+	return celeb_recognition(image_path)
