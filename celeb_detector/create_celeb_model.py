ef7a375e0a55079cf48f317c1c8d34368adbbe47
adbmdp <alexis.de.bretagne@gmail.com>
2024-05-02 11:25:26 +0200

    Update create_celeb_model to save json just once

--- celeb_detector/create_celeb_model.py
+++ celeb_detector/create_celeb_model.py
@@ -19,42 +19,40 @@ def save_json(celeb_mapping):
 		json.dump(celeb_mapping, outfile)
 
 def create_celeb_model(base_path):
-	ann_index = AnnoyIndex(2048, 'angular')
-	os.makedirs('celeb_encodings', exist_ok=True)
+    ann_index = AnnoyIndex(2048, 'angular')
+    os.makedirs('celeb_encodings', exist_ok=True)
+    celeb_mapping = {}
+    celeb_encoding = {}
+    c = 0
+    print("Starting face detection and encoding creation")
+    for folder in os.listdir(base_path):
+        celeb_mapping[folder] = []
+        for image_name in tqdm(os.listdir(os.path.join(base_path, folder))):
+            image = cv2.imread(os.path.join(base_path, folder, image_name))
+            try:
+                encodings, bboxes = celeb_recog.get_encoding(image)
+            except Exception as e:
+                print(e)
+                continue
+            if encodings is not None:
+                for encoding, bbox in zip(encodings, bboxes):
+                    c += 1
+                    celeb_encoding[c] = encoding.squeeze()
+                    celeb_mapping[folder].append(c)
+                    ann_index.add_item(c, encoding.squeeze())
+            with open(f"celeb_encodings/{folder}_encoding.pkl", "wb") as f:
+                pickle.dump(celeb_encoding, f)
+            celeb_encoding.clear()
+    print("Encoding and mapping files saved successfully")
+    print("Building ann index...")
+    ann_index.build(1000)
+    x = ann_index.save("celeb_index.ann")
+    if x:
+        print("Ann index saved successfully")
+    else:
+        print("Error in saving ann index")
 
-	celeb_mapping = {}
-	c = 0
-	print("Starting face detection and encoding creation")
-	for folder in os.listdir(base_path):
-		celeb_encoding = {}
-		celeb_mapping[folder] = []
-		for image_name in tqdm(os.listdir(os.path.join(base_path, folder))):
-			image = cv2.imread(os.path.join(base_path, folder, image_name))
-			try:
-				encoding = celeb_recog.get_encoding(image)
-			except Exception as e:
-				print(e)
-				continue
-
-			if encoding is not None:
-				c += 1
-				celeb_encoding[c] = encoding[0]
-				celeb_mapping[folder].append(c)
-				ann_index.add_item(c, encoding[0])
-		save_json(celeb_mapping)
-		pickle.dump(celeb_encoding, open(f"celeb_encodings/{folder}_encoding.pkl", "wb" ))
-		del celeb_encoding
-
-	save_json(celeb_mapping)
-	print("Encoding and mapping files saved successfully")
-
-	print("Building ann index...")
-	ann_index.build(1000)
-	x = ann_index.save("celeb_index.ann")
-	if x:
-		print("Ann index saved successfully")
-	else:
-		print("Error in saving ann index")
+    save_json(celeb_mapping)
 
 if __name__ == "__main__":
 	create_model = create_celeb_model("")
